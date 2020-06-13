138ee0b75a58ea402201099bbb747be92361399b
Evgeniy Semenov <SemenovEvg1990@gmail.com>
2020-06-13 17:00:19 +0300

    Учтены комментарии. Код только для меток.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 'use strict';
 
-document.querySelector('.map').classList.remove('map--faded');
-
 var NUMBER_OBJ = 8;
-var IMG_AVATAR = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png'
-];
+var NUMBER_X = 100;
+var NUMBER_XX = 1100;
+var NUMBER_Y = 130;
+var NUMBER_YY = 630;
 var TITLE_ARR = ['Заголовок1', 'Заголовок2', 'Заголовок3'];
-var ADDRESS_ARR = ['адрес 300', 'адрес 400', 'адрес 600'];
-var PRICE_ARR = ['800 руб', '1000 руб', '2000 руб'];
+var PRICE_ARR = ['800', '1000', '2000'];
 var TYPE_ARR = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS_ARR = ['1', '2', '3'];
 var GUESTS_ARR = ['1', '2', '3'];
@@ -31,11 +22,10 @@ var PHOTOS_ARR = [
 
 // создаем переменную с элементом, куда копировать
 var mapListElement = document.querySelector('.map__pins');
-// создаем переменную с шаблоном, который копировать
-/* var cardTemplate = document.querySelector('#card')
-    .content
-    .querySelector('.map__card');*/
-var pinTemplate = document.querySelector('#pin')
+var mapList = document.querySelector('.map');
+// создаем переменные с шаблоном, которые копировать
+
+var pinTemplate = document.querySelector('#pin') // метка
     .content
     .querySelector('.map__pin');
 
@@ -43,50 +33,58 @@ var pinTemplate = document.querySelector('#pin')
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+// функиция вызова рандомных елементов
+var getRandomElement = function (array) {
+  return array[[getRandomInRange(0, array.length - 1)]];
+};
 
 // создаём массив объявлений с уникальными характеристиками
 var adverts = [];
 
-for (var i = 0; i < NUMBER_OBJ; i++) {
+for (var i = 1; i <= NUMBER_OBJ; i++) {
+  var locationX = getRandomInRange(NUMBER_X, NUMBER_XX);
+  var locationY = getRandomInRange(NUMBER_Y, NUMBER_YY);
   adverts.push({
     author: {
-      avatar: IMG_AVATAR[getRandomInRange(0, IMG_AVATAR.length - 1)]
+      avatar: 'img/avatars/user0' + i + '.png'
     },
     offer: {
-      title: TITLE_ARR[getRandomInRange(0, TITLE_ARR.length - 1)],
-      address: ADDRESS_ARR[getRandomInRange(0, ADDRESS_ARR.length - 1)],
-      price: PRICE_ARR[getRandomInRange(0, PRICE_ARR.length - 1)],
-      type: TYPE_ARR[getRandomInRange(0, TYPE_ARR.length - 1)],
-      rooms: ROOMS_ARR[getRandomInRange(0, ROOMS_ARR.length - 1)],
-      guests: GUESTS_ARR[getRandomInRange(0, GUESTS_ARR.length - 1)],
-      checkin: CHECKIN_ARR[getRandomInRange(0, CHECKIN_ARR.length - 1)],
-      checkout: CHECKOUT_ARR[getRandomInRange(0, CHECKOUT_ARR.length - 1)],
-      features: FEATURES_ARR[getRandomInRange(0, FEATURES_ARR.length - 1)],
-      desccription: DESCCRIPTION_ARR[getRandomInRange(0, DESCCRIPTION_ARR.length - 1)],
-      photos: PHOTOS_ARR[getRandomInRange(0, PHOTOS_ARR.length - 1)],
+      title: getRandomElement(TITLE_ARR),
+      address: locationX + ', ' + locationY,
+      price: getRandomElement(PRICE_ARR),
+      type: getRandomElement(TYPE_ARR),
+      rooms: getRandomElement(ROOMS_ARR),
+      guests: getRandomElement(GUESTS_ARR),
+      checkin: getRandomElement(CHECKIN_ARR),
+      checkout: getRandomElement(CHECKOUT_ARR),
+      features: getRandomElement(FEATURES_ARR),
+      desccription: getRandomElement(DESCCRIPTION_ARR),
+      photos: getRandomElement(PHOTOS_ARR),
     },
     location: {
-      x: getRandomInRange(0, 1200), // х - ограничено размерами блока
-      y: getRandomInRange(130, 630) //  y - от 130 до 630
+      x: locationX, // х - ограничено размерами блока
+      y: locationY //  y - от 130 до 630
     }
   });
 }
 
 // функция отрисовки меток
-var renderPin = function (advert) {
+var renderPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.querySelector('img').src = advert.offer.photos;
-  pinElement.querySelector('img').alt = advert.offer.title;
-  pinElement.style.left = advert.location.x + 'px';
-  pinElement.style.top = advert.location.y + 'px';
+  pinElement.querySelector('img').src = pin.offer.photos;
+  pinElement.querySelector('img').alt = pin.offer.title;
+  pinElement.style.left = pin.location.x + 'px';
+  pinElement.style.top = pin.location.y + 'px';
 
   return pinElement;
 };
 
 // создаем фрагмент дома, который будет добавлять
-var fragment = document.createDocumentFragment();
+var fragmentPin = document.createDocumentFragment();
 for (var j = 0; j < adverts.length; j++) {
-  fragment.appendChild(renderPin(adverts[j]));
+  fragmentPin.appendChild(renderPin(adverts[j]));
 }
-mapListElement.appendChild(fragment);
+mapListElement.appendChild(fragmentPin);
+
+mapList.classList.remove('map--faded');
