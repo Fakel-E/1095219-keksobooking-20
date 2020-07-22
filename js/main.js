ada2b805013fa4338e00e0c78ce8e7e635ffbd17
Evgeniy Semenov <SemenovEvg1990@gmail.com>
2020-07-22 13:02:16 +0300

    Изменил на удаление обработчика

--- js/main.js
+++ js/main.js
@@ -2,23 +2,25 @@
 
 var mapListElement = document.querySelector('.map__pins');
 var mainButton = document.querySelector('.map__pin--main');
-var mapList = document.querySelector('.map');
 
-if (mapList.classList.contains('map--faded')) {
-  mainButton.addEventListener('click', function () {
-    // открываем карту по клику
-    window.map.activateMap();
+var cancelLoad = function () {
+  mainButton.removeEventListener('click', function () {});
+};
 
-    window.load(function (adverts) {
-      var fragment = document.createDocumentFragment();
+mainButton.addEventListener('click', function () {
+  // открываем карту по клику
+  window.map.activateMap();
 
-      for (var i = 0; i < adverts.length; i++) {
-        fragment.appendChild(window.pin.renderPin(adverts[i]));
-      }
-      mapListElement.appendChild(fragment);
-    }, function () { });
-  });
-}
+  window.load(function (adverts) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < adverts.length; i++) {
+      fragment.appendChild(window.pin.renderPin(adverts[i]));
+    }
+    mapListElement.appendChild(fragment);
+  }, function () { });
+  cancelLoad();
+});
 
 /* var form = userDialog.querySelector('.setup-wizard-form');
 form.addEventListener('submit', function (evt) {
