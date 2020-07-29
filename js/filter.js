9c61166cd560325d6a985ac2b6942a604703be31
Evgeniy Semenov <SemenovEvg1990@gmail.com>
2020-07-29 19:09:05 +0300

    Попытка сортировки

--- js/filter.js
+++ js/filter.js
@@ -3,6 +3,9 @@
 (function () {
   var mapFilters = document.querySelector('.map__filters');
   var house = mapFilters.querySelector('#housing-type');
+  var price = mapFilters.querySelector('#housing-price');
+  var guest = mapFilters.querySelector('#housing-guests');
+  var room = mapFilters.querySelector('#housing-rooms');
 
   var houseType = function (pins) {
     var sameHouseType = pins.filter(function (it) {
@@ -11,7 +14,48 @@
     return sameHouseType;
   };
 
+  var priceType = function (pins) {
+    var PriceLevel = {
+      LOW: 9999,
+      MIDDLE: 49999,
+      HIGH: 50000
+    };
+    var samePriceType = pins.filter(function (it) {
+      if (it.offer.price < PriceLevel.LOW) {
+
+        return it.offer.price < PriceLevel.LOW;
+      } else if (PriceLevel.LOW < it.offer.price && it.offer.price <= PriceLevel.MIDDLE) {
+
+        return it.offer.price > PriceLevel.LOW && it.offer.price <= PriceLevel.MIDDLE;
+      } /* else if (it.offer.price >= PriceLevel.HIGH) {
+        console.log('3');
+        return it.offer.price >= PriceLevel.HIGH;
+      }*/
+      return it.offer.price === price.value;
+    });
+
+    return samePriceType;
+  };
+
+  var guestType = function (pins) {
+    var sameGuestType = pins.filter(function (it) {
+      return it.offer.guests === Number(guest.value);
+    });
+    return sameGuestType;
+  };
+
+  var roomType = function (pins) {
+    var sameRoomType = pins.filter(function (it) {
+      return it.offer.rooms === Number(room.value);
+    });
+    return sameRoomType;
+  };
+
   window.filter = {
-    houseType: houseType
+    houseType: houseType,
+    priceType: priceType,
+    guestType: guestType,
+    roomType: roomType
+
   };
 })();
--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@
   var mainButton = document.querySelector('.map__pin--main');
   var mapFilters = document.querySelector('.map__filters');
   var house = mapFilters.querySelector('#housing-type');
+  var price = mapFilters.querySelector('#housing-price');
+  var guest = mapFilters.querySelector('#housing-guests');
+  var room = mapFilters.querySelector('#housing-rooms');
+  // var features = mapFilters.querySelector('#housing-features');
+
   var arrayAdverts = [];
 
   var onMainButtonClick = function () {
@@ -31,6 +36,66 @@
       mapCard.remove();
     }
   });
+
+  price.addEventListener('change', function () {
+    var newAdverts = window.filter.priceType(arrayAdverts);
+    var mapCard = document.querySelector('.map__card');
+    window.pin.deleteMarks('.map__pin');
+    window.pin.renderMarks(newAdverts);
+    if (price.value !== 'any') {
+      window.pin.renderMarks(newAdverts);
+    } else {
+      window.pin.renderMarks(arrayAdverts);
+    }
+    if (mapCard) {
+      mapCard.remove();
+    }
+  });
+
+  guest.addEventListener('change', function () {
+    var newAdverts = window.filter.guestType(arrayAdverts);
+    var mapCard = document.querySelector('.map__card');
+    window.pin.deleteMarks('.map__pin');
+    window.pin.renderMarks(newAdverts);
+    if (guest.value !== 'any') {
+      window.pin.renderMarks(newAdverts);
+    } else {
+      window.pin.renderMarks(arrayAdverts);
+    }
+    if (mapCard) {
+      mapCard.remove();
+    }
+  });
+
+  room.addEventListener('change', function () {
+    var newAdverts = window.filter.roomType(arrayAdverts);
+    var mapCard = document.querySelector('.map__card');
+    window.pin.deleteMarks('.map__pin');
+    window.pin.renderMarks(newAdverts);
+    if (room.value !== 'any') {
+      window.pin.renderMarks(newAdverts);
+    } else {
+      window.pin.renderMarks(arrayAdverts);
+    }
+    if (mapCard) {
+      mapCard.remove();
+    }
+  });
+
+  /* features.addEventListener('change', function () {
+    var newAdverts = window.filter.featuresType(arrayAdverts);
+    var mapCard = document.querySelector('.map__card');
+    window.pin.deleteMarks('.map__pin');
+    window.pin.renderMarks(newAdverts);
+    if (room.value !== 'any') {
+      window.pin.renderMarks(newAdverts);
+    } else {
+      window.pin.renderMarks(arrayAdverts);
+    }
+    if (mapCard) {
+      mapCard.remove();
+    }
+  });*/
 
 
   var form = document.querySelector('.ad-form');
